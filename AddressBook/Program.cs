8ad589c89affb885f9ad3c123ef2f5ebfd1b77cb
akshata_rn <akshatarn123@gamail.com>
2022-11-24 20:56:05 +0530

    Creating contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -8,9 +8,29 @@ namespace AddressBook
 {
     internal class Program
     {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Address Book Program!\n");
+        }
+        public static void CreatingContact()
+        {
+            Console.WriteLine("Enter your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your Phone_Number");
+            double phoneNo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your Email_Id");
+            string emailId = Console.ReadLine();
+            Console.WriteLine($"\n First Name : {firstName}\n Last Name : {lastName}\n Address : {address}\n Phone Number : {phoneNo}\n Email ID : {emailId}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            WelcomeMessage();
+            CreatingContact();
         }
     }
 }
