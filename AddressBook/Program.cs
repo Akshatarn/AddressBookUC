208ea9bf1d85459a0caa2d47ac9ef57c4ea21f23
akshata_rn <akshatarn123@gamail.com>
2022-11-24 21:10:18 +0530

    Editing existing contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -12,24 +12,28 @@ namespace AddressBook
         {
             ContactList contact = new ContactList();
             contact.WelcomeMessage();
-        ReEnteringApp:
+        ReEnteringApp:                        
             Console.WriteLine("Select any one from below options \n" +
                 "Press 1 : to Create/Add a new contact\n" +
-                "Press 2 : to view all the contacts in Address book.");
+                "Press 2 : to Edit/Modify any existing contact\n" +
+                "Press 3 : to view all the contacts in Address book.");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    contact.AddPerson();
+                    contact.AddPerson();        
                     break;
                 case 2:
-                    contact.ListPeople();
+                    contact.EditPerson();       
+                    break;
+                case 3:
+                    contact.ListPeople();       
                     break;
                 default:
                     Console.WriteLine("Invalid Option selected , Please try again ");
                     break;
             }
-            goto ReEnteringApp;
+            goto ReEnteringApp;     //for repeatation of same task from start point have used goto control statement .
         }
     }
 }
