302be153017f7c84b0cef1ebc76f0faf30907e7f
akshata_rn <akshatarn123@gamail.com>
2022-11-24 21:23:20 +0530

    Adding Dictionary

--- AddressBook/Person.cs
+++ AddressBook/Person.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class Person
+    public class Person
     {
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string address { get; set; }
         public double phoneNo { get; set; }
         public string emailId { get; set; }
+
+        public Person(string firstName, string lastName, string address, double phoneNo, string emailId)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.phoneNo = phoneNo;
+            this.emailId = emailId;
+        }
+        public Person()
+        {
+
+        }
     }
 }
